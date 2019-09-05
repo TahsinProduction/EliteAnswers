0d07a430f7b2630150b7111974fb8ac3571803e3
TahsinProduction <khantahsinkabir@gmail.com>
2019-09-06 00:07:07 +0600

    Initial

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const AboutPage = () => (
+  <Layout pageTitle="Contact Us">
+    <SEO title="ContactUS" keywords={[`gatsby`, `application`, `react`]} />
+    <div className="col-md-3"></div>
+    <img src = "https://lh3.googleusercontent.com/9liGmQurv_ClCtUes9BVsfaFrpuODInWPKSvO_KbyBCBqq8Zub6JU8tJL5CK_zvJTqyUN9Uy6Hhd6GPcNAJ8JvXyMjjrlUE8V1EfzWj8usCdnyIJAyWDGke8LU0xwBKIu_mqUkeq" width="100%" height="auto" margin="0 auto" alt="embed" />
+<h1>If you have any query or problem or want to say something to us then email us at</h1><p>(<strong>tahsinproduction1@gmail.com</strong>)</p><br></br><h1>You will receive reply shortly</h1>
+</Layout>)
+export default AboutPage
